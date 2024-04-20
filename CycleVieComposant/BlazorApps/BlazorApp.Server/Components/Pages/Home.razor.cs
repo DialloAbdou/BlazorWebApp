0123429ab6a281bec15e12e8aee7f718009c3ee3
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-04-20 23:39:20 +0200

    Onitialized

--- CycleVieComposant/BlazorApps/BlazorApp.Server/Components/Pages/Home.razor.cs
+++ CycleVieComposant/BlazorApps/BlazorApp.Server/Components/Pages/Home.razor.cs
@@ -5,9 +5,11 @@ using System.Runtime.ExceptionServices;
 namespace BlazorApp.Server.Components.Pages;
 public partial class Home
 {
-
+    private string _apiResponse; 
+    List<string> _steps = new List<string>();
     protected override void OnInitialized()
     {
+        _steps.Add(nameof(OnInitialized));
         Console.WriteLine("OnInitialized -DEBUT");
         base.OnInitialized();
         Console.WriteLine("OnInitialized -FIN");
@@ -15,18 +17,23 @@ public partial class Home
     }
     protected override async Task OnInitializedAsync()
     {
+        _steps.Add(nameof(OnInitializedAsync));
         Console.WriteLine("OnInitializedAsync -DEBUT");
         await base.OnInitializedAsync();
+        await Task.Delay(15000);
+        _apiResponse = "Réponse de l'api";
         Console.WriteLine("OnInitializedAsync -FIN");
     }
     protected override void OnParametersSet()
     {
+        _steps.Add(nameof(OnParametersSet));
         Console.WriteLine("OnParametersSet -DEBUT");
         base.OnParametersSet();
         Console.WriteLine("OnParametersSet -FIN");
     }
     protected override async Task OnParametersSetAsync()
     {
+        _steps.Add(nameof(OnParametersSetAsync));
         Console.WriteLine("OnParametersSetAsync -DEBUT");
         await base.OnParametersSetAsync();
         Console.WriteLine("OnParametersSetAsync -FIN");
@@ -34,21 +41,24 @@ public partial class Home
 
     protected override void OnAfterRender(bool firstRender)
     {
-        Console.WriteLine( $"OnAfterRender -DEBUT (First ? {firstRender})");
+        _steps.Add(nameof(OnAfterRender));
+        Console.WriteLine($"OnAfterRender -DEBUT (First ? {firstRender})");
         base.OnAfterRender(firstRender);
         Console.WriteLine($"OnAfterRender -FIN (First ? {firstRender})");
 
     }
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
+        _steps.Add(nameof(OnAfterRenderAsync));
         Console.WriteLine($"OnAfterRenderAsync -DEBUT (First ? {firstRender})");
-        await  base.OnAfterRenderAsync(firstRender);
+        await base.OnAfterRenderAsync(firstRender);
         Console.WriteLine($"OnAfterRenderAsync -FIN (First ? {firstRender})");
 
     }
 
     protected override bool ShouldRender()
     {
+        _steps.Add(nameof(ShouldRender));
         Console.WriteLine($"ShouldRender -DEBUT");
         Console.WriteLine($"ShouldRender -FIN ");
         return base.ShouldRender();
