dd5c6150f338d8555c1766494a8ea3af061c3e6b
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-04-26 22:26:34 +0200

    afterRender

--- CycleVieComposant/BlazorApps/BlazorApp.Shared/LifeCycle.razor.cs
+++ CycleVieComposant/BlazorApps/BlazorApp.Shared/LifeCycle.razor.cs
@@ -8,6 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorApp.Shared
 {
     public partial class LifeCycle
@@ -15,7 +17,7 @@ namespace BlazorApp.Shared
 
         private string _apiResponse;
         List<string> _steps = new List<string>();
-        private string ? _var= "Valeur Initiale";
+        private string? _var = "Valeur Initiale";
         protected override void OnInitialized()
         {
             _steps.Add(nameof(OnInitialized));
@@ -29,8 +31,8 @@ namespace BlazorApp.Shared
             _steps.Add(nameof(OnInitializedAsync));
             Console.WriteLine("OnInitializedAsync -DEBUT");
             await base.OnInitializedAsync();
-            await Task.Delay(15000);
-            _apiResponse = "Réponse de l'api";
+            //await Task.Delay(15000);
+            //_apiResponse = "Réponse de l'api";
             Console.WriteLine("OnInitializedAsync -FIN");
         }
         protected override void OnParametersSet()
@@ -48,6 +50,7 @@ namespace BlazorApp.Shared
             Console.WriteLine("OnParametersSetAsync -FIN");
         }
 
+
         protected override void OnAfterRender(bool firstRender)
         {
             _steps.Add(nameof(OnAfterRender));
@@ -60,6 +63,14 @@ namespace BlazorApp.Shared
         {
             _steps.Add(nameof(OnAfterRenderAsync));
             Console.WriteLine($"OnAfterRenderAsync -DEBUT (First ? {firstRender})");
+            if (firstRender)
+            {
+                await Task.Delay(5000);
+                _apiResponse = "Réponse de l'api";
+                StateHasChanged(); // permet de raflichir sans qu'on click sur le bouton de refresh il faut qu'il soit toujours dans une condition
+
+            }
+
             await base.OnAfterRenderAsync(firstRender);
             Console.WriteLine($"OnAfterRenderAsync -FIN (First ? {firstRender})");
 
@@ -71,6 +82,7 @@ namespace BlazorApp.Shared
             Console.WriteLine($"ShouldRender -DEBUT");
             Console.WriteLine($"ShouldRender -FIN ");
             return base.ShouldRender();
+        
         }
     }
 }
