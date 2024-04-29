5c1ddf49fcbe839d264ac2d1d588991b2afac7c1
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-04-29 16:02:42 +0200

    Structure du projet PhotoExplorer

---
+++ TravauPratique/PhotoExplorer/PhotoExplorer/PhotoExplorer.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PhotoExplorer.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
